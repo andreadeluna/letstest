f60887982a1de2b3a12a9593eb9772e0dd94b9a8
andreadeluna <andrea.deluna10@gmail.com>
2021-08-21 15:50:36 +0200

    Aggiunta splash screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:progettotirocinio/screens/form_screen.dart';
 import 'screens/home.dart';
@@ -21,7 +23,75 @@ class App extends StatelessWidget {
           headline6: TextStyle(color: Colors.red),
         ),
       ),
-      home: FormScreen(status, cronologia),
+      //home: FormScreen(status, cronologia),
+      home: SplashScreen(status, cronologia),
     );
   }
 }
+
+class SplashScreen extends StatefulWidget {
+  String status;
+  String cronologia;
+
+  SplashScreen(this.status, this.cronologia);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState(status, cronologia);
+}
+
+
+// *** LINK VIDEO: https://www.youtube.com/watch?v=PQB1E2JLZJA ***
+
+class _SplashScreenState extends State<SplashScreen> {
+  String status;
+  String cronologia;
+
+  _SplashScreenState(this.status, this.cronologia);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), (){
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  FormScreen(status, cronologia)));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('images/splash.png', height: 150),
+            SizedBox(height: 40),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*class  extends StatefulWidget {
+  const ({Key? key}) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}*/
+
