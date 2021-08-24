78d3bcd578fc9a0c287897dc807f67b524864ca8
andreadeluna <andrea.deluna10@gmail.com>
2021-08-24 02:30:22 +0200

    Modificata grafica schermata inserimento nominativo

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:progettotirocinio/screens/home.dart';
 
@@ -26,8 +28,8 @@ class FormScreenState extends State<FormScreen> {
 
   Widget _buildName() {
     return TextFormField(
-      decoration: InputDecoration(labelText: 'Nome'),
-      maxLength: 10,
+      decoration: InputDecoration(labelText: 'Nome e cognome'),
+      //maxLength: 10,
       validator: (String value) {
         if (value.isEmpty) {
           return 'Il nome è richiesto';
@@ -85,53 +87,119 @@ class FormScreenState extends State<FormScreen> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      //resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text(
-          'Progetto Tirocinio',
-          style: TextStyle(color: Colors.white, fontSize: 24.0),
-        ),
-        backgroundColor: Colors.lightBlue,
-      ),
-      body: Center(
-        child: Container(
-          margin: EdgeInsets.all(25),
-          child: Form(
-              key: _formKey,
-              child: ListView(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 30),
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+          Colors.lightBlue[800],
+          Colors.lightBlue[700],
+          Colors.lightBlue[300],
+        ])),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 40),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  _buildName(),
-                  _buildEmail(),
-                  _buildPhoneNumber(),
-                  SizedBox(height: 50),
-                  RaisedButton(
-                    child: Text(
-                      'Invia',
-                      style: TextStyle(color: Colors.blue, fontSize: 16),
-                    ),
-                    onPressed: () {
-                      if (!_formKey.currentState.validate()) {
-                        return;
-                      }
-
-                      _formKey.currentState.save();
-                      focusStat =
-                          'DATI:\nNome: $_name\nEmail: $_email\nTelefono: $_phonenumber\n\n  ';
-                      status += focusStat;
-                      print(_name);
-                      print(_email);
-                      print(_phonenumber);
-                      print(status);
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  HomePage(status + 'INVIO DATI\n\n  ', cronologia)));
-                    },
+                  Text(
+                    "Inserimento nominativo",
+                    style: TextStyle(color: Colors.white, fontSize: 40),
                   )
                 ],
-              )),
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                      topRight: Radius.circular(60),
+                      bottomLeft: Radius.circular(60),
+                      bottomRight: Radius.circular(60)),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 60),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.lightBlue[100],
+                                blurRadius: 20,
+                                offset: Offset(0, 10),
+                              )
+                            ]),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom:
+                                          BorderSide(color: Colors.grey[200]))),
+                              child: Form(
+                                key: _formKey,
+                                child: Column(children: <Widget>[
+                                  _buildName(),
+                                  _buildEmail(),
+                                  _buildPhoneNumber(),
+                                ]),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 50),
+                      GestureDetector(
+                        onTap: () {
+                          if (!_formKey.currentState.validate()) {
+                            return;
+                          }
+
+                          _formKey.currentState.save();
+                          focusStat =
+                              'DATI:\nNome: $_name\nEmail: $_email\nTelefono: $_phonenumber\n\n  ';
+                          status += focusStat;
+                          print(_name);
+                          print(_email);
+                          print(_phonenumber);
+                          print(status);
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => HomePage(
+                                  status + 'INVIO DATI\n\n  ', cronologia),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          height: 50,
+                          margin: EdgeInsets.symmetric(horizontal: 50),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: Colors.lightBlue[900]),
+                          child: Center(
+                              child: Text("Invia", style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold), ),
+                              ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
